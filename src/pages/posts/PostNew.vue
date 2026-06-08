7363cc33585f18e5b028f516247d42447d2edc02
LuckyGuy-777 <yusch98@naver.com>
2026-06-08 18:09:42 +0900

    260608 [develop] 게시글 작성페이지 작성 중  (PostNew  UserInfo )

---
+++ src/pages/posts/PostNew.vue
@@ -0,0 +1,23 @@
+<script setup>
+import MyButton from '../../components/button/MyButton.vue';
+
+
+</script>
+
+<template>
+  
+
+
+
+<MyButton
+  :color="'gray'"
+  :size="'middle'"
+  :content="'Write'"
+  @click="getNextPage()" 
+/>
+
+</template>
+
+<style scoped>
+
+</style>
